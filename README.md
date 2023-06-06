8a41edc0867080756e59ed2ec84a5a52f9102de4
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-06 10:49:05 +0530

    Add .env file, requirements.txt and README.md file

---
+++ README.md
@@ -0,0 +1,7 @@
+## A simple web app to understand basic concepts of django like url routing, views, rendering HTTP responses and templates etc.
+
+### You can enter some text in text area and can select the operations like removing punctuations,removing spaces and capitalizing, and those operations will be performed. 
+
+### Used some bootstrap elements. Again it's just a simple app with no much logic or functions. Nothing much to expect.
+
+### Thanks
--- text_analyzer_project/settings.py
+++ text_analyzer_project/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-usn2ckc0du9$xa5k=i)%fvb0t7jc8vzs9ggdv$bvtf)-_0hji0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
